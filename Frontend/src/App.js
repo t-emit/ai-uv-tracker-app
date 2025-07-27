29bf2dbbb510ae865bc9b9f686d320e876197535
t-emit <t.siddiqui2814@gmail.com>
2025-07-27 21:23:26 +0530

    feat: Host model on H for reliable download

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route , Navigate } from 'react-router-dom';
 
 // Import all your components
 import Navbar from './components/Navbar';
